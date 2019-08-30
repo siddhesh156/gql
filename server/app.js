15aeab05c5c34aa13b7523e4f62c1a98c7aec732
siddhesh <siddhesh@siddheshs-MacBook-Air.local>
2019-08-30 16:34:59 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+//connect to mlab
+mongoose.connect('mongodb+srv://shaun:shaun1234@gpl-sid-2ew5y.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
+
+app.use('/graphql', graphqlHTTP({
+schema,
+graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for request on port 4000');
+});
