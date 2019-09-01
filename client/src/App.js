b15e029f4497d50ecb7c70e9b368c6d4c97d1e2c
siddhesh <siddhesh@siddheshs-MacBook-Air.local>
2019-09-01 14:39:42 +0530

    28 Book and Author Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BookList from './components/BookList'
 import  ApolloClient  from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import AddBook from './components/AddBook';
 
 //apollo client setup
 const client = new ApolloClient({
@@ -16,6 +17,7 @@ function App() {
       <div className="main">
         <h1>Hello World</h1>
         <BookList />
+        <AddBook />
       </div>
     </ApolloProvider>
   );
